de341c64287e05f978d34fbd4042e08197ebe7e5
LukMcCall <luk1555@wp.pl>
2019-04-21 19:03:49 +0200

    OpenCV better init func and system detection

---
+++ Web/RecognizerLib/src/main/java/pl/sudokusolver/recognizerlib/InitClass.java
@@ -0,0 +1,48 @@
+package pl.sudokusolver.recognizerlib;
+
+import org.opencv.core.Core;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Locale;
+
+public class InitClass {
+    public enum OSType {
+        Windows, MacOS, Linux, Other
+    }
+
+    // cached result of OS detection
+    private static OSType detectedOS;
+
+
+    public static void init(){
+        System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
+    }
+
+    public static void init(String url){
+        String libPath = Paths.get(url,Core.NATIVE_LIBRARY_NAME).toString();
+        switch (getOperatingSystemType()){
+            case Windows:
+                libPath += ".dll";
+            break;
+            //todo: add linux support
+        }
+        System.load(libPath);
+    }
+
+    public static OSType getOperatingSystemType() {
+        if (detectedOS == null) {
+            String OS = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
+            if ((OS.contains("mac")) || (OS.contains("darwin"))) {
+                detectedOS = OSType.MacOS;
+            } else if (OS.contains("win")) {
+                detectedOS = OSType.Windows;
+            } else if (OS.contains("nux")) {
+                detectedOS = OSType.Linux;
+            } else {
+                detectedOS = OSType.Other;
+            }
+        }
+        return detectedOS;
+    }
+}
--- Web/RecognizerLib/src/main/java/pl/sudokusolver/recognizerlib/test.java
+++ Web/RecognizerLib/src/main/java/pl/sudokusolver/recognizerlib/test.java
@@ -1,21 +1,19 @@
 package pl.sudokusolver.recognizerlib;
 
 import pl.sudokusolver.recognizerlib.digitsrecognizers.ANN;
+import pl.sudokusolver.recognizerlib.digitsrecognizers.IRecognizer;
 import pl.sudokusolver.recognizerlib.gridrecognizers.DigitBoxByteSum;
 import pl.sudokusolver.recognizerlib.gridrecognizers.Grid;
 import pl.sudokusolver.recognizerlib.gridrecognizers.SudokuDetector;
 
 public class test {
 
-    static {
-        System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
-    }
-
     public static void main(String[] args) {
         System.out.println("Rec Lib Test");
 
+        InitClass.init("C:\\opencv4.0.1\\opencv\\build\\java\\x64");
 
-        ANN ann = new ANN("RecognizerLib/ann.xml");
+        IRecognizer ann = new ANN("RecognizerLib/ann.xml");
 
         SudokuDetector sudokuDetector = new SudokuDetector(ann, new DigitBoxByteSum());
         Grid grid = new Grid();
