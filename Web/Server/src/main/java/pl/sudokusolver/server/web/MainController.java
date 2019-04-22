1c026b8e73bc13b0694921d8945efeb132f0fd37
LukMcCall <luk1555@wp.pl>
2019-04-22 16:41:43 +0200

    Adding and conf MultipartResolver
    To be honest i don't know what i did :cry:
    But it's working :D

--- Web/Server/src/main/java/pl/sudokusolver/server/web/MainController.java
+++ Web/Server/src/main/java/pl/sudokusolver/server/web/MainController.java
@@ -2,13 +2,24 @@ package pl.sudokusolver.server.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import pl.sudokusolver.recognizerlib.sudoku.Sudoku;
 
 @Controller
 public class MainController {
     @RequestMapping(value = "/")
     public String home(Model model) {
+        return "home";
+    }
+
+    @RequestMapping(value = "solve", method = RequestMethod.POST)
+    public String solve(@RequestParam("sudoku")
+                                MultipartFile multipart, Model model){
+        System.out.println("Upload" + multipart.getOriginalFilename());
         return "home";
     }
 }
--- Web/Server/src/main/java/pl/sudokusolver/server/web/WebConfig.java
+++ Web/Server/src/main/java/pl/sudokusolver/server/web/WebConfig.java
@@ -1,7 +1,9 @@
 package pl.sudokusolver.server.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,5 +16,12 @@ public class WebConfig implements WebMvcConfigurer{
     public void configureViewResolvers(ViewResolverRegistry registry) {
         // directory to views folder
         registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+
+    @Bean(name="multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multi = new CommonsMultipartResolver();
+        multi.setMaxUploadSize(10000000); //todo: change it
+        return multi;
     }
 }
