41edb71518d70f282fe75301c1bde0e5816c8d86
nkwasniak <48225976+nkwasniak@users.noreply.github.com>
2019-05-27 01:57:47 +0200

    GameBoardTest.java
    Major psznie je 1:57.

--- App/src/main/java/pl/sudokusolver/app/GameBoardTest.java
+++ App/src/main/java/pl/sudokusolver/app/GameBoardTest.java
@@ -23,18 +23,16 @@ class GameBoardTest {
 
     int[][] solution = new int[][]
             {
-                {5,3,8,4,6,1,7,9,2},
-                {6,9,7,3,2,5,8,1,4},
-                {2,1,4,7,8,9,5,6,3},
-                {9,4,1,2,7,8,6,3,5},
-                {7,6,2,1,5,3,9,4,8},
-                {8,5,3,9,4,6,1,2,7},
-                {3,8,9,5,1,2,4,7,6},
-                {4,2,6,8,9,7,3,5,1},
-                {1,7,5,6,3,4,2,8,9}
+                    {5,3,8,4,6,1,7,9,2},
+                    {6,9,7,3,2,5,8,1,4},
+                    {2,1,4,7,8,9,5,6,3},
+                    {9,4,1,2,7,8,6,3,5},
+                    {7,6,2,1,5,3,9,4,8},
+                    {8,5,3,9,4,6,1,2,7},
+                    {3,8,9,5,1,2,4,7,6},
+                    {4,2,6,8,9,7,3,5,1},
+                    {1,7,5,6,3,4,2,8,9}
             };
-
-
 
 
     @Test
@@ -147,10 +145,104 @@ class GameBoardTest {
 
     @Test
     void checkForSuccess() {
+        GameBoard gameBoard = new GameBoard();
+
+        int[][] myInitial = new int[][]
+                {
+                        {5,3,8,4,6,1,7,9,2},
+                        {6,9,7,3,2,5,8,1,4},
+                        {2,1,4,7,8,9,5,6,3},
+                        {9,4,1,2,7,8,6,3,5},
+                        {7,6,2,1,5,3,9,4,8},
+                        {8,5,3,9,4,6,1,2,7},
+                        {3,8,9,5,1,2,4,7,6},
+                        {4,2,6,8,9,7,3,5,1},
+                        {1,7,5,6,3,4,2,8,9}
+                };
+
+        gameBoard.modifyInitial(myInitial);
+        Assert.assertTrue(gameBoard.checkForSuccess());
+
+        gameBoard.modifyInitial(0,5,5);
+        gameBoard.modifyInitial(0,2,0);
+        gameBoard.modifyInitial(0,0,2);
+
+        int[][] myPlayer = new int[][]
+                {
+                        {5,3,8,4,6,1,7,9,2},
+                        {6,9,7,3,2,5,8,1,4},
+                        {2,1,4,7,8,9,5,6,3},
+                        {9,4,1,2,7,8,6,3,5},
+                        {7,6,2,1,5,3,9,4,8},
+                        {8,5,3,9,4,0,1,2,7},
+                        {3,8,9,5,1,2,4,7,6},
+                        {4,2,6,8,9,7,3,5,1},
+                        {1,7,5,6,3,4,2,8,9}
+                };
+
+        gameBoard.modifyPlayer(myPlayer);
+
+        Assert.assertFalse(gameBoard.checkForSuccess());
+
     }
 
     @Test
     void checkForSuccessGeneral() {
+
+        GameBoard gameBoard = new GameBoard();
+
+        int[][] myInitial = new int[][]
+                {
+                        {0,0,8,0,6,1,7,0,2},
+                        {6,9,7,3,2,5,8,1,4},
+                        {2,0,4,7,8,9,5,6,3},
+                        {0,4,1,2,0,8,0,3,5},
+                        {7,6,0,1,5,3,9,0,8},
+                        {8,0,3,0,4,6,1,0,7},
+                        {0,8,0,5,1,2,0,7,0},
+                        {4,0,6,8,9,7,0,5,1},
+                        {0,7,0,6,0,4,2,8,0}
+                };
+
+
+        int[][] myPlayer = new int[][]
+                {
+                        {5,3,0,4,0,0,0,9,0},
+                        {0,0,0,0,0,0,0,0,0},
+                        {0,1,0,0,0,0,0,0,0},
+                        {9,0,0,0,7,0,6,0,0},
+                        {0,0,2,0,0,0,0,4,0},
+                        {0,5,0,9,0,0,0,2,0},
+                        {3,0,9,0,0,0,4,0,6},
+                        {0,2,0,0,0,0,3,0,0},
+                        {1,0,5,0,3,0,0,0,9}
+                };
+
+        gameBoard.modifyPlayer(myPlayer);
+        gameBoard.modifyInitial(myInitial);
+
+        Assert.assertTrue(gameBoard.checkForSuccessGeneral());
+
+
+        myPlayer = new int[][]
+                {
+                        {5,3,0,1,0,0,0,9,0},
+                        {0,0,0,0,0,0,0,0,0},
+                        {0,1,0,0,0,0,0,0,0},
+                        {9,0,0,0,7,0,6,0,0},
+                        {0,0,2,0,0,0,0,4,0},
+                        {0,5,0,9,0,0,0,2,0},
+                        {3,0,9,0,0,0,2,0,6},
+                        {0,2,0,0,0,0,3,0,0},
+                        {1,0,5,0,3,0,0,0,9}
+                };
+
+        gameBoard.modifyPlayer(myPlayer);
+        gameBoard.modifyInitial(myInitial);
+
+        Assert.assertFalse(gameBoard.checkForSuccessGeneral());
+
+
     }
 
 }
